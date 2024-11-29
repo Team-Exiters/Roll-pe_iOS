9032bcc594996b5ae1d220063afb9d1517c38a88
hugesilver <xor3160@naver.com>
2024-11-30 03:46:25 +0900

    Initial Commit

---
+++ Roll-Pe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Roll-Pe
+//
+//  Created by 김태은 on 11/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Roll-PeTests/Roll_PeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Roll_PeTests.swift
+//  Roll-PeTests
+//
+//  Created by 김태은 on 11/30/24.
+//
+
+import Testing
+@testable import Roll_Pe
+
+struct Roll_PeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
