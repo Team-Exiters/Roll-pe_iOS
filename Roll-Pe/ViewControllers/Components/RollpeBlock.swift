10e301fe5d8a215bc2ec82c9eddf3f31b2a5adff
hugesilver <xor3160@naver.com>
2025-01-23 05:09:27 +0900

    Refactor: Improve MainBeforeSignInViewController code

--- Roll-Pe/ViewControllers/Components/RollpeBlock.swift
+++ Roll-Pe/ViewControllers/Components/RollpeBlock.swift
@@ -8,76 +8,72 @@
 import UIKit
 import SnapKit
 
-class RollpeBlock: UIView {
-    init(frame: CGRect = .zero, image: String, titleText: String) {
-        super.init(frame: frame)
-        
-        self.layer.cornerRadius = 16
-        self.layer.borderWidth = 2
-        self.layer.borderColor = UIColor.rollpeSecondary.cgColor
-        
-        self.snp.makeConstraints { make in
-            make.width.equalTo(170)
-            make.height.equalTo(199)
-        }
-        
-        let contentView: UIStackView = UIStackView()
-        
-        contentView.translatesAutoresizingMaskIntoConstraints = false
-        contentView.axis = .vertical
-        contentView.spacing = 0
-        contentView.alignment = .center
-        contentView.distribution = .fill
-        
-        self.addSubview(contentView)
-        
-        contentView.snp.makeConstraints { make in
-            make.top.equalToSuperview().inset(24)
-            make.horizontalEdges.equalToSuperview()
-            make.width.equalToSuperview()
-            make.height.equalToSuperview()
-        }
-        
-        // MARK: - 이미지
-        let imageView: UIImageView = UIImageView()
-        let image: UIImage! = UIImage(named: image)
-        imageView.image = image
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.contentMode = .scaleAspectFit
-        imageView.clipsToBounds = true
-        
-        contentView.addArrangedSubview(imageView)
-        
-        // 가로형
-        imageView.snp.makeConstraints { make in
-            make.horizontalEdges.equalToSuperview().inset(16)
-            make.width.greaterThanOrEqualTo(1)
-            make.height.equalTo(imageView.snp.width).dividedBy(getImageRatio(image: image!))
-        }
-        
-        // MARK: - 제목
-        
-        let title: UILabel = UILabel()
-        title.textColor = .rollpeSecondary
-        title.font = UIFont(name: "HakgyoansimDunggeunmisoOTF-R", size: 16)
-        title.numberOfLines = 1
-        title.textAlignment = .center
-        title.lineBreakMode = .byTruncatingTail
-        title.text = titleText
-        
-        contentView.addArrangedSubview(title)
-        
-        title.snp.makeConstraints { make in
-            make.width.equalToSuperview().inset(16)
-            make.top.greaterThanOrEqualTo(8)
-            make.horizontalEdges.equalToSuperview().inset(16)
-            make.centerX.equalToSuperview()
-        }
-        
-        contentView.setCustomSpacing(24, after: title)
+func RollpeBlock(image: String, titleText: String) -> UIView {
+    let view: UIView = UIView()
+    
+    view.layer.cornerRadius = 16
+    view.layer.borderWidth = 2
+    view.layer.borderColor = UIColor.rollpeSecondary.cgColor
+    
+    view.snp.makeConstraints { make in
+        make.width.equalTo(170)
+        make.height.equalTo(199)
     }
     
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
+    let contentView: UIStackView = UIStackView()
+    
+    contentView.translatesAutoresizingMaskIntoConstraints = false
+    contentView.axis = .vertical
+    contentView.spacing = 0
+    contentView.alignment = .center
+    contentView.distribution = .fill
+    
+    view.addSubview(contentView)
+    
+    contentView.snp.makeConstraints { make in
+        make.top.equalToSuperview().inset(24)
+        make.horizontalEdges.equalToSuperview()
+        make.width.equalToSuperview()
+        make.height.equalToSuperview()
     }
+    
+    // MARK: - 이미지
+    let imageView: UIImageView = UIImageView()
+    let image: UIImage! = UIImage(named: image)
+    imageView.image = image
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    imageView.contentMode = .scaleAspectFit
+    imageView.clipsToBounds = true
+    
+    contentView.addArrangedSubview(imageView)
+    
+    // 가로형
+    imageView.snp.makeConstraints { make in
+        make.horizontalEdges.equalToSuperview().inset(16)
+        make.width.greaterThanOrEqualTo(1)
+        make.height.equalTo(imageView.snp.width).dividedBy(getImageRatio(image: image!))
+    }
+    
+    // MARK: - 제목
+    
+    let title: UILabel = UILabel()
+    title.textColor = .rollpeSecondary
+    title.font = UIFont(name: "HakgyoansimDunggeunmisoOTF-R", size: 16)
+    title.numberOfLines = 1
+    title.textAlignment = .center
+    title.lineBreakMode = .byTruncatingTail
+    title.text = titleText
+    
+    contentView.addArrangedSubview(title)
+    
+    title.snp.makeConstraints { make in
+        make.width.equalToSuperview().inset(16)
+        make.top.greaterThanOrEqualTo(8)
+        make.horizontalEdges.equalToSuperview().inset(16)
+        make.centerX.equalToSuperview()
+    }
+    
+    contentView.setCustomSpacing(24, after: title)
+    
+    return view
 }
