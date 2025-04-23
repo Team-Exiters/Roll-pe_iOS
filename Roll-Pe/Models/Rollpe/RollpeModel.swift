09efaa1550d8de8ba15a5deeed28beea2509d5ea
hugesilver <xor3160@naver.com>
2025-04-23 19:05:23 +0900

    Chore: Remove old RollpeModel

--- Roll-Pe/Models/Rollpe/RollpeModel.swift
+++ Roll-Pe/Models/Rollpe/RollpeModel.swift
@@ -7,18 +7,6 @@
 
 import Foundation
 
-struct RollpeModel {
-    // api값 저장만하고 화면에 뿌려주는 용도라 id추가 필요없음
-    // 변수들 작명,타입은 임의, 추후에 최종수정
-    let host : String //host는 임의로 넣어둠
-    let title : String
-    var writers : [UserDataModel]
-    var participants : [UserDataModel]
-    var isPublic : Bool
-    var date : Date
-    var password : String?
-}
-
 // 롤페 목록 모델
 struct RollpeResponseListModel: Decodable {
     let status_code: Int
